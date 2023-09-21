a41ce71ebe98412b4f081c331f20612c070c698f
SEFSIS <sofiagryn@icloud.com>
2023-09-21 23:14:02 +0300

    lab1

--- src/Lab1HrynKI305.java
+++ src/Lab1HrynKI305.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 /**
  * Програма для створення квадратної матриці і збереження її у файлі.
  */
-public class Main {
+public class Lab1HrynKI305 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -14,32 +14,25 @@ public class Main {
         System.out.print("Введіть розмір квадратної матриці: ");
         int n = scanner.nextInt();
 
-        String filler1, filler2;
+        String filler;
         do {
-            // Запит на введення символів-заповнювачів
-            System.out.print("Введіть символ-заповнювач 1 (символ довжиною 1): ");
-            filler1 = scanner.next();
-            System.out.print("Введіть символ-заповнювач 2 (символ довжиною 1): ");
-            filler2 = scanner.next();
-
-            if (filler1.length() != 1 || filler2.length() != 1) {
-                System.out.println("Символи-заповнювачі мають бути лише одним символом кожен.");
+            // Запит на введення символу-заповнювача
+            System.out.print("Введіть символ-заповнювач (символ довжиною 1): ");
+            filler = scanner.next();
+            if (filler.length() != 1) {
+                System.out.println("Символ-заповнювач має бути лише одним символом.");
             }
-        } while (filler1.length() != 1 || filler2.length() != 1);
-
-        char fillerChar1 = filler1.charAt(0);
-        char fillerChar2 = filler2.charAt(0);
+        } while (filler.length() != 1);
+        char fillerChar = filler.charAt(0);
 
         // Створення та заповнення матриці
         char[][] matrix = new char[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (i >= j && i + j >= n - 1) {
-                    matrix[i][j] = fillerChar1; // Перший символ
-                } else if (i <= j && i + j <= n - 1) {
-                    matrix[i][j] = fillerChar2; // Другий символ
+                if (i >= j && i + j >= n - 1 || i <= j && i + j <= n - 1) {
+                    matrix[i][j] = fillerChar;
                 } else {
-                    matrix[i][j] = ' '; // Нічого (пробіл)
+                    matrix[i][j] = ' ';
                 }
             }
         }
